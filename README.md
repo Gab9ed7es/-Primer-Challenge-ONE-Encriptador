51f41ce8eee7c8b0839d33db1aca548215f8f2e1
Gabriel Escobar <gabi180020@gmail.com>
2022-05-30 20:54:19 -0300

    PrimerChallengeONE

---
+++ README.md
@@ -0,0 +1,3 @@
+# PrimerChallengeONE
+<h3>El desafío consiste en encriptar el texto ingresado por el usuario y así poder intercambiar mensajes secretos con otros usuarios que
+  conozcan la encriptación utilizada.</h3>
---
+++ controller/encripted.js
@@ -0,0 +1,56 @@
+const entrada = document.querySelector('#entrada')
+const salida = document.querySelector('#txt2')
+entrada.focus();
+
+
+function ocultar () {
+    var muñeco = document.querySelector("#muñeco");
+    var txt1 = document.querySelector("#txt1");
+    var txt2 = document.querySelector("#txt2");
+    var copyBtn = document.querySelector("#copyAcc");
+
+    txt1.classList.add("invisible");
+    muñeco.classList.add("invisible");
+    txt2.classList.add("mostrar");
+    copyBtn.classList.remove("invisible");
+
+}
+
+const lowerCaseCorrection = (str) => {
+    return str.toLowerCase();
+}
+
+const removeAccents = (str) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  } 
+
+function uiEncriptar (){
+    ocultar();
+    let texto = document.querySelector("#entrada").value;
+    let textoV = document.getElementById("entrada");
+    let lowTexto = lowerCaseCorrection(texto);
+    let cadena = removeAccents(lowTexto);
+    textoV.value = cadena;
+    let textoCifrado = cadena.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+    document.getElementById("txt2").textContent = textoCifrado;
+}
+
+function uiDesencriptar () {
+    ocultar();
+    var texto = document.querySelector("#entrada").value;
+    var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    document.getElementById("txt2").textContent = textoCifrado;
+}
+
+function copyToClipBoard() {
+    let entrada  = document.querySelector("#entrada");
+    entrada.value = "";
+
+    var copyText = document.getElementById("txt2");
+  
+    console.log(copyText);
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+  
+    navigator.clipboard.writeText(copyText.value);
+  }
